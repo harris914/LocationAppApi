706f16dd7c1c5ac29597a8f3a8c19ed06d7bb71f
harrisnaeem <harris@innostark.com>
2017-03-27 23:07:30 +0500

    First Commit

---
+++ WebApi/WebApi/ModelMappers/UserMapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApi.WebModels;
+
+namespace WebApi.ModelMappers
+{
+    public static class UserMapper
+    {
+        public static User CreateFrom(this Models.User source)
+        {
+            return new User
+            {
+                Id = source.Id,
+                Name = source.Name,
+                Username = source.Username,
+                Age = source.Age,
+                Contact = source.Contact,
+                City = source.City,
+                Country = source.Country,
+                Latitude = source.Latitude,
+                Longitude = source.Longitude,
+                Region = source.Region,
+                Password = source.Password,
+                ImageUrl = source.ImageUrl,
+                TimeFrom = source.TimeFrom,
+                TimeTo = source.TimeTo,
+                Services = source.Services.Select(x=>x.CreateFrom()).ToList(),
+                HourlyRate = source.HourlyRate,
+                
+            };
+        }
+    }
+}
---
+++ WebApi/WebApi/WebModels/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.WebModels
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int? Age { get; set; }
+        public string Contact { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Region { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public TimeSpan? TimeFrom { get; set; }
+        public TimeSpan? TimeTo { get; set; }
+        public string ImageUrl { get; set; }
+        public double? HourlyRate { get; set; }
+        public List<Service> Services { get; set; }
+    }
+}
